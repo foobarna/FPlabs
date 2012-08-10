5509c350deff62bd50629f5758764e8b77de1af7
Bogdan BaRna <b.bogdu@gmail.com>
2012-08-10 13:33:20 +0300

    added problem no.4 for lab12

---
+++ lab12ex4/ex4.py
@@ -0,0 +1,49 @@
+#4. The sequence  a a an
+#, ,...,
+#1 2
+# with integer elements is given. Determine all strictly increasing
+#subsequences of sequence a (preserving the order of elements in the original sequence).
+
+def print_it(sol, n):
+    for i in range(1, n + 1):
+        print sol[i],
+    return
+
+def succesor(n, k, sol):
+    if sol[k] < 9 :
+        sol[k] = sol[k] + 1;
+        return True
+    return False
+
+def is_valid(k, sol):
+    if sol[1] == 0 : return False
+    x = k / 2 + 1
+    for j in range (1, x):
+        y = k - j + 1
+        for i in range (1, y):
+            a = i
+            b = i + j
+            c = i + j
+            d = i + j + j
+            if sol[a:b] == sol[c:d] : return False
+    return True
+
+def is_solution(k, n):
+    return (k == n + 1)
+
+def bcktr(n, k, sol):
+    if is_solution(k, n):
+        print_it(sol, n)
+        print '\n'
+    else :
+        sol[k] = -1
+        while succesor(n, k, sol) :
+            if is_valid(k, sol) : bcktr(n, k + 1, sol)
+
+def main():
+    sol = []
+    n = input('n=')
+    for i in range(n + 1): sol.append(i);
+    bcktr(n, 1, sol)
+
+main()
